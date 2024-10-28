dbae955353f85dc970e7945e4821db7789c46946
mael <maelgarnier5@gmail.com>
2024-10-28 15:33:56 -0400

    Codage du jeu GuessMyNumber

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+
+int userNumber = 0;
+int computerNumber = random.Next(1, 11);
+string? userInput;
+bool userHasWon = false;
+
+Console.WriteLine("Entrez un nombre entre 1 et 10");
+
+while (!userHasWon)
+{
+    userInput = Console.ReadLine();
+    if (userInput == null) Console.WriteLine("Il faut entrer quelque chose");
+    else if (!int.TryParse(userInput, out userNumber)) Console.WriteLine("Il faut rentrer un nombre");
+    else
+    {
+        userHasWon = int.Parse(userInput) == computerNumber;
+        if (!userHasWon) Console.WriteLine("Perdu");
+    }
+}
+
+Console.WriteLine("Gagné");
